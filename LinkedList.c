bebab32bdb28eacee6235da1384b1bde6c06d129
André Cirne <dumahk21@gmail.com>
2017-03-06 23:30:16 +0000

    Metodos da linked list funcionam todos

--- LinkedList.c
+++ LinkedList.c
@@ -18,33 +18,34 @@ List *newList(){
   List *lista;
   lista = (List *)malloc(sizeof(List));
   lista -> size = 0;
+  lista -> first;
   lista -> end = NULL;
 }
 
-Item *newItem(){
+Item *newItem(Monom value){
   Item *newp;
   newp=(Item*)malloc(sizeof(Item));
-  newp -> value = newMonom();
+  newp -> value = value
   newp -> next = NULL;
   return newp;
 }
 
-void add(List *lista){
+void add(List *lista, Monom value){
   Item *newp = lista -> end;
 
   //criacao do novo item
   if(newp == NULL){
-    lista -> end = newItem();
+    lista -> first = lista -> end = newItem(value);
   }
   else{
-    newp -> next = newItem();
+    newp -> next = newItem(value);
     lista -> end = newp -> next;
   }
   //incremento do tamanho da lista
   lista -> size = lista -> size + 1;
 }
 
-void remove(List *lista, int index){
+void removeItem(List *lista, int index){
 
   //verificar se o array encontra s edentros do limites
   if(lista->size<=index){
@@ -76,3 +77,31 @@ void remove(List *lista, int index){
   }
   lista -> size = lista -> size--;
 }
+
+/*
+Debug of list
+void printList(List *lista){
+  Item *newp = lista -> first;
+  while (newp != NULL) {
+    printf(" %d\n",newp->value );
+    newp = newp -> next;
+  }
+  printf("\n");
+}
+
+int main(int argc, char const *argv[]) {
+  int n;
+  List *lista = newList();
+  scanf("%d",&n );
+  while (n != -1) {
+    add(lista,n);
+    scanf("%d",&n );
+  }
+  printList(lista);
+  removeItem(lista,0);
+  printList(lista);
+
+
+  return 0;
+}
+*/
--- LinkedList.h
+++ LinkedList.h
@@ -1,13 +1,15 @@
 //
 // LinkedList header
 //
+#pragma once
 
 #include <stdlib.h>
+#include "Monomio.h"
 
 typedef struct List List;
 typedef struct Item Item;
 
 List *newList();
-Item *newItem();
-void add(List *lista);
-void remove(List *lista, int index);
+Item *newItem(Monom value);
+void add(List *lista, Monom value);
+void removeItem(List *lista, int index);
